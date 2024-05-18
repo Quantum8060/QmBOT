b33ba09890121594dd3e06d146412ac1d75c683d
Quantum8060 <akaunnt8060@gmail.com>
2024-05-19 00:49:23 +0900

    mcstatusをブラックリストに対応

--- main.py
+++ main.py
@@ -631,27 +631,35 @@ async def help(ctx):
 #Minecraft　server status
 @bot.slash_command(name='mcstatus', description="マイクラのサーバーステータスを確認します。")
 async def mcstatus(ctx, server_address):
-    url = f"https://api.mcstatus.io/v2/status/java/{server_address}"
-    response = requests.get(url)
+    user_id = str(ctx.author.id)
 
-    if response.status_code == 200:
-        data = response.json()
+    data = load_data()
+
+    if user_id not in data:
+
+        url = f"https://api.mcstatus.io/v2/status/java/{server_address}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
         
-        if data['online']:
-            embed = discord.Embed(title="Minecraft Server Status", description=f"サーバーアドレス: {server_address}", color=discord.Color.green())
-            embed.add_field(name="オンライン", value="Yes", inline=True)
-            embed.add_field(name="ホスト", value=data['host'], inline=True)
-            embed.add_field(name="ポート", value=data['port'], inline=True)
-            embed.add_field(name="バージョン", value=data['version']['name_clean'], inline=True)
-            embed.add_field(name="プレイヤー数", value=f"{data['players']['online']} / {data['players']['max']}", inline=True)
-            embed.add_field(name="MOTD", value=data['motd']['clean'], inline=False)
+            if data['online']:
+                embed = discord.Embed(title="Minecraft Server Status", description=f"サーバーアドレス: {server_address}", color=discord.Color.green())
+                embed.add_field(name="オンライン", value="Yes", inline=True)
+                embed.add_field(name="ホスト", value=data['host'], inline=True)
+                embed.add_field(name="ポート", value=data['port'], inline=True)
+                embed.add_field(name="バージョン", value=data['version']['name_clean'], inline=True)
+                embed.add_field(name="プレイヤー数", value=f"{data['players']['online']} / {data['players']['max']}", inline=True)
+                embed.add_field(name="MOTD", value=data['motd']['clean'], inline=False)
+            else:
+                embed = discord.Embed(title="Minecraft Server Status", description=f"サーバーアドレス: {server_address}", color=discord.Color.red())
+                embed.add_field(name="オンライン", value="No", inline=True)
+        
+            await ctx.respond(embed=embed, ephemeral=True)
         else:
-            embed = discord.Embed(title="Minecraft Server Status", description=f"サーバーアドレス: {server_address}", color=discord.Color.red())
-            embed.add_field(name="オンライン", value="No", inline=True)
-        
-        await ctx.respond(embed=embed, ephemeral=True)
+            await ctx.respond(f"サーバー情報の取得に失敗しました: HTTP {response.status_code}", ephemeral=True)
     else:
-        await ctx.respond(f"サーバー情報の取得に失敗しました: HTTP {response.status_code}", ephemeral=True)
+        await ctx.response.send_message("あなたはブラックリストに登録されています。", ephemeral=True)
 
 
 
