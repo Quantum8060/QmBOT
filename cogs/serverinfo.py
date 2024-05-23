86c66c208c207ab465ee26d4b3dcec99321a01db
Quantum8060 <akaunnt8060@gmail.com>
2024-05-24 01:10:37 +0900

    serverinfoをcogs化

---
+++ cogs/serverinfo.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+Debug_guild = [1235247721934360577]
+
+class serverinfo(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @discord.slash_command(name="serverinfo", description="サーバーの情報を表示します。")
+    async def serverinfo(self, interaction: discord.ApplicationContext):
+    
+        embed = discord.Embed(title="サーバー情報", color=0x4169e1)
+        embed.set_author(name=f"{interaction.guild.name}")
+        embed.add_field(name="所有者", value=f"{interaction.guild.owner.mention}", inline=False)
+        embed.add_field(name="id", value=f"{interaction.guild.id}", inline=False)
+        embed.add_field(name="メンバー数", value=f"{interaction.guild.member_count}", inline=False)
+        embed.add_field(name="サーバー作成日", value=f"{interaction.guild.created_at}", inline=False)
+        embed.set_footer(text=f"{interaction.user.display_name}", icon_url=interaction.user.avatar.url)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+def setup(bot):
+    bot.add_cog(serverinfo(bot))
