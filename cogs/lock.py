e9f1242ad03449ed1a7b1fe118ebb918684b66d5
Quantum8060 <akaunnt8060@gmail.com>
2024-06-10 19:28:55 +0900

    lockの修正

--- cogs/lock.py
+++ cogs/lock.py
@@ -4,23 +4,13 @@ import json
 
 Debug_guild = [1235247721934360577]
 
-blacklist_file = 'blacklist.json'
-
-def load_data():
-    with open(blacklist_file, 'r') as file:
-        return json.load(file)
-
-def save_data(data):
-    with open(blacklist_file, 'w') as file:
-        json.dump(data, file, indent=4)
-
 lock_file = 'lock.json'
 
-def load_lock_data():
+def load_data():
     with open(lock_file, 'r') as file:
         return json.load(file)
 
-def save_lock_data(data):
+def save_data(data):
     with open(lock_file, 'w') as file:
         json.dump(data, file, indent=4)
 
@@ -32,26 +22,19 @@ class lock(commands.Cog):
 
     @discord.slash_command(name="lock", description="サーバーをロックします。")
     @commands.is_owner()
-    async def lock(self, interaction: discord.Interaction, reason: discord.Option(str, description="理由を入力します。")):
-        b_id = str(interaction.author.id)
+    async def lock(self, interaction: discord.ApplicationContext, reason: discord.Option(str, description="理由を入力します。")):
         l_id = str(interaction.guild.id)
 
-        b_data = load_blacklist_data()
-        data = load_lock_data()
+        data = load_data()
 
-        if b_id not in data:
+        if l_id not in data:
+            await interaction.response.send_message(f"サーバー:{l_id}をロックしました。", ephemeral=True)
 
-            data = load_lock_data()
-
-            if l_id not in data:
-                await interaction.respond(f"サーバー:{l_id}をロックしました。", ephemeral=True)
-
-                data[str(interaction.guild.id)] = reason
-                save_lock_data(data)
-            else:
-                await interaction.response.send_message("このサーバーはすでにロックされています。", ephemeral=True)
+            data[str(interaction.guild.id)] = reason
+            save_data(data)
         else:
-            await interaction.response.send_message("あなたはブラックリストに登録されています。", ephemeral=True)
+            await interaction.response.send_message("このサーバーはすでにロックされています。", ephemeral=True)
+
 
 def setup(bot):
     bot.add_cog(lock(bot))
