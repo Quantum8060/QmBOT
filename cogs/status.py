3d4964ec13ce07727c2a8d6a61d40eaacc2e4f9c
Quantum8060 <akaunnt8060@gmail.com>
2024-06-24 12:47:03 +0900

    ステータスコマンドをサブコマンド化

--- cogs/status.py
+++ cogs/status.py
@@ -1,36 +1,29 @@
 import discord
 from discord.ext import commands
+from discord.commands import SlashCommandGroup
 
 Debug_guild = [1235247721934360577]
 
-class dnd(commands.Cog):
+class status(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-    @discord.slash_command(name='set_dnd', description="取り込み中にします。")
+    statuses = SlashCommandGroup("status", "ステータスグループ")
+
+    @statuses.command(name='dnd', description="取り込み中にします。")
     @commands.is_owner()
     async def dnd(self, interaction: discord.ApplicationContext):
         await self.bot.change_presence(status = discord.Status.dnd)
         await interaction.response.send_message("取り込み中に変更しました。", ephemeral=True)
 
-class idle(commands.Cog):
-
-    def __init__(self, bot):
-        self.bot = bot
-
-    @discord.slash_command(name='set_idle', description="退出中にします。")
+    @statuses.command(name='idle', description="退出中にします。")
     @commands.is_owner()
     async def idle(self, interaction: discord.ApplicationContext):
         await self.bot.change_presence(status = discord.Status.idle)
         await interaction.response.send_message("退席中に変更しました。", ephemeral=True)
 
-class online(commands.Cog):
-
-    def __init__(self, bot):
-        self.bot = bot
-
-    @discord.slash_command(name='set_online', description="オンラインにします。")
+    @statuses.command(name='online', description="オンラインにします。")
     @commands.is_owner()
     async def online(self, interaction: discord.ApplicationContext):
         await self.bot.change_presence(status = discord.Status.online)
@@ -39,6 +32,4 @@ class online(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(dnd(bot))
-    bot.add_cog(idle(bot))
-    bot.add_cog(online(bot))
+    bot.add_cog(status(bot))
