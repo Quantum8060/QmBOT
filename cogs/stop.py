84fa4bc9abccc0bcb0e000d3444c61b851d82a35
Quantum8060 <akaunnt8060@gmail.com>
2024-11-22 10:16:44 +0900

    再起動実装

--- cogs/stop.py
+++ cogs/stop.py
@@ -4,6 +4,7 @@ import os
 from time import sleep
 from discord.ext import tasks
 import asyncio
+import subprocess
 
 Debug_guild = [1235247721934360577]
 
@@ -21,6 +22,20 @@ class stop(commands.Cog):
         await asyncio.sleep(1)
         loop = asyncio.get_event_loop()
         loop.stop
+
+    @discord.slash_command(name="reboot", description="BOTを再起動します。")
+    @commands.is_owner()
+    async def reboot(self, ctx):
+
+        embed = discord.Embed(title="再起動確認", description="BOTを再起動します。")
+        await ctx.respond(embed=embed)
+        await self.bot.close()
+        loop = asyncio.get_event_loop()
+        loop.stop
+        await asyncio.sleep(30)
+        await subprocess.call(['pm2', 'restart' 'main'])
+
+
 
 
 def setup(bot):
